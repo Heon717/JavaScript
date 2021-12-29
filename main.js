1b1d4d50f2bbb5bb1e0feabfe1815610906e2582
Heon <fiia2001@naver.com>
2021-12-29 18:03:48 +0900

    Tab 구현

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-let black_background = document.querySelector('.black-background');
-let nav_sub_btn = document.querySelector('.nav-sub-btn');
-let modal_close = document.querySelector('.modal-close');
-let nav_sub = document.getElementById('nav-sub');
-let form_email = document.querySelector('.form-email');
-let form_password = document.querySelector('.form-password');
-let alert_email = document.querySelector('.alert-email');
-let alert_password = document.querySelector('.alert-password');
+const black_background = document.querySelector('.black-background');
+const nav_sub_btn = document.querySelector('.nav-sub-btn');
+const modal_close = document.querySelector('.modal-close');
+const nav_sub = document.getElementById('nav-sub');
+const form_email = document.querySelector('.form-email');
+const form_password = document.querySelector('.form-password');
+const alert_email = document.querySelector('.alert-email');
+const alert_password = document.querySelector('.alert-password');
 
 nav_sub.style.opacity = 0.1;
 
@@ -68,8 +68,35 @@ nav_sub_btn.addEventListener('click',function(e){
     },100)
 })
 
-let slide_2 = document.querySelector('.slide-2');
-let slide_container = document.querySelector('.slide-container');
-slide_2.addEventListener('click', function() {
-    // slide_container.animate.
+const slide_2 = document.querySelector('.slide-2');
+const slide_container = document.querySelector('.slide_container');
+const nav_menu = document.querySelector('.nav-menu');
+const large_logo = document.querySelector('.large-logo');
+
+window.addEventListener('scroll',function() {
+        if (window.scrollY > nav_menu.offsetHeight) {
+            nav_menu.className = 'nav-black';
+            large_logo.className = 'small-logo';
+        } else {
+            nav_menu.classList.remove('nav-black');
+            nav_menu.className = 'nav-menu';
+            large_logo.className = 'large-logo';
+
+        }
 })
+
+const tab_button = document.querySelectorAll('.tab-button');
+const tab_content = document.querySelectorAll('.tab-content');
+
+// for (let i = 0; i < tab_button.length; i++) {
+//     tab_button[i].addEventListener('click',function() {;
+//     tab_button.classList.remove('active');
+//     tab_content.classList.remove('show');
+//     tab_button[i].className = 'active';
+//     tab_content[i].className = 'show';
+// });
+// }
+// 바닐라js로는 아직 구현하지못하였음..
+
+// 버튼을 클릭하면 active 클래스가 생성 되고 , 기존 active 가 있던 클래스를 삭제한다.
+
